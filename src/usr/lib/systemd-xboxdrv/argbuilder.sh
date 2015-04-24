23771deea9faa281b2b7d60fa0ee005971ad58af
gyates100895 <gyates100895@gmail.com>
2015-04-24 01:11:08 -0400

    Create argbuilder.sh

---
+++ src/usr/lib/systemd-xboxdrv/argbuilder.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CONFIGPATH="/etc/xboxdrv"
+RUNPATH="/var/run/xboxdrv/$1"
+ID="$(ls $RUNPATH)"
+ARGS="--config ${CONFIGPATH}/${ID}.xboxdrv"
+for configfile in /etc/xboxdrv/${ID}/*; do
+    if [ "${configfile}" = "${CONFIGPATH}/${ID}/config0.xboxdrv" ]; then
+        ARGS="$ARGS --config $configfile"
+    else
+        ARGS="$ARGS --alt-config $configfile"
+    fi
+done
+echo $ARGS > /var/run/xboxdrv/$1/args
